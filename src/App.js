5558205005db5626f20086aaaa6b2b74e43165d8
Ahmad khatab <dev.ahmad.khattab@gmail.com>
2023-01-09 21:21:49 +0300

    add missing stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import Hellotext from "@hellotext/hellotext";
 
 function App() {
+  window.Hellotext = Hellotext
   return (
     <div className="App">
       <header className="App-header">
