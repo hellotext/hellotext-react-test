69f828356cc8f4da8ddb1fd79eca115f4d8f106a
rockwellll <dev.ahmad.khattab@gmail.com>
2024-07-02 18:54:50 +0300

    update code

--- src/App.js
+++ src/App.js
@@ -1,38 +1,58 @@
+import { useEffect, useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 import Hellotext from "@hellotext/hellotext";
 
-Hellotext.on("session-set", async (session) => {
-  console.log("session is set")
-  const response = await Hellotext.track("app.installed", {
-    app_parameters: {
-      name: `Hellotext ${new Date().toString()}`
-    }
-  })
+// Hellotext.on("session-set", async (session) => {
+//   console.log("session is set")
+//   const response = await Hellotext.track("app.installed", {
+//     app_parameters: {
+//       name: `Hellotext ${new Date().toString()}`
+//     }
+//   })
+//
+//   console.log(response)
+//   console.log(response.data)
+// })
 
-  console.log(response)
-  console.log(response.data)
+Hellotext.on('form:completed', (form) => {
+  console.log("form completed")
+  console.log(form)
 })
 
-// Hellotext.initialize("BUSINESS_ID")
+Hellotext.initialize("zGrDJ1Lb", {
+  autogenerateSession: false,
+  apiRoot: 'http://api.lvh.me:3000/v1'
+})
 
 function App() {
   window.Hellotext = Hellotext
+  const [forms, setForms] = useState([])
+
+  Hellotext.on("forms:collected", (forms) => {
+    setForms(Hellotext.forms.forms)
+  })
+
+  useEffect(() => {
+    Hellotext.forms.collect()
+  }, [])
+
+  console.log(forms)
   return (
     <div className="App">
+
+      <form data-hello-form="zGrDJ1Lb">
+      </form>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          forms.map(form => (
+           <button onClick={() => Hellotext.forms.forms[0].mount()}>
+            Load Form
+          </button>
+        ))
+        }
       </header>
     </div>
   );
